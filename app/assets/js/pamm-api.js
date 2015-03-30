7f74be6b5a1adcf5c653cd7d35ac9667146b9387
Mike Hadrup <github@hadrup.com>
2015-03-31 04:27:45 +1300

    Enable PA Server Mod Manager only when scenes are used
    - adds a check for server mod scenes to determine if the PA Server Mod
    Manager should be enabled
    - additional logging and error checking

--- app/assets/js/pamm-api.js
+++ app/assets/js/pamm-api.js
@@ -745,13 +745,15 @@ var _updateFiles = function(context) {
     {
 
         pamm_path = paths.pamm_server;
-        
 
         // server version of ui_mod_list.js loads local client copy of ui_mod_list_for_server.js then merges server scenes into client scenes
-        
+
         uimodlist = "var global_server_mod_list = " + JSON.stringify(globalmodlist, null, 4) + ";\n\nvar scene_server_mod_list = " + JSON.stringify(scenemodlist, null, 4) + ";\n\ntry { \n\nloadScript('coui://ui/mods/ui_mod_list_for_server.js');\n\ntry { global_mod_list = _.union( global_mod_list, global_server_mod_list ) } catch (e) { console.log(e); } ;\n\ntry { _.forOwn( scene_server_mod_list, function( value, key ) { if ( scene_mod_list[ key ] ) { scene_mod_list[ key ] = _.union( scene_mod_list[ key ], value ) } else { scene_mod_list[ key ] = value } } ); } catch (e) { console.log(e); } \n\n\} catch (e) {\n\nconsole.log(e);\n\nvar global_mod_list = global_server_mod_list;\n\nvar scene_mod_list = scene_server_mod_list;\n\n}\n\n";
 
         // for server mods we only enable the PA Server Mod Manager if there are other server mods enabled that use scenes
+
+        var sceneCount = globalmodlist.length + _.flatten( _.values( scenemodlist ) ).length;
+        
         jsAddLogMessage( 'Found ' + sceneCount + ' scenes in ' + context + ' mods.json', 4);
         
          if ( sceneCount == 0 )
